8d69595aae29b18eb29f3dc3cc420e1de0260758
sangho <sh21kang@naver.com>
2018-08-10 09:32:56 +0900

    final version

---
+++ components/AppContainer/presenter.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { View, Text, StatusBar, StyleSheet } from "react-native";
+import LoggedOutNavigation from "../../navigation/LoggedOutNavigation";
+import RootNavigation from "../../navigation/RootNavigation";
+
+class AppContainer extends Component {
+  static propTypes = {
+    isLoggedIn: PropTypes.bool.isRequired,
+    initApp: PropTypes.func.isRequired
+  };
+  componentDidMount() {
+    const { isLoggedIn, initApp } = this.props;
+    if (isLoggedIn) {
+      initApp();
+    }
+  }
+  render() {
+    const { isLoggedIn, profile } = this.props;
+    return (
+      <View style={styles.container}>
+        <StatusBar hidden={false} />
+        {isLoggedIn && profile ? (
+          <RootNavigation screenProps={{ username: profile.username }} />
+        ) : (
+          <LoggedOutNavigation />
+        )}
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff"
+  }
+});
+
+export default AppContainer;
---
+++ constants.js
@@ -0,0 +1,2 @@
+export const API_URL = "http://57d3945a.ngrok.io";
+export const FB_APP_ID = "246956852777624";
